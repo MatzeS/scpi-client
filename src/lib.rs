7cd9c348e39692fc051e040f487b9c678f0c5ad1
Matthias Sokolowski <matthias.sokolowski@gmail.com>
2025-08-07 22:08:21 +0200

    refactor: move scpi_enum to dedicated file

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 use std::str::pattern::{Pattern, Searcher};
 use thiserror::Error;
 
+pub mod enumerations;
 pub mod primitives;
 
 #[derive(Error, Debug)]
@@ -151,48 +152,6 @@ pub fn check_empty(input: &str) -> Result<()> {
             "Response should be empty/fully deserialized, but still has content: `{input}`"
         )))
     }
-}
-
-#[macro_export]
-macro_rules! scpi_enum {
-    (
-        $(#[$enum_meta:meta])*
-        $vis:vis enum $name:ident {
-            $(
-                $(#[$variant_meta:meta])*
-                $variant:ident => $literal:expr
-            ),* $(,)?
-        }
-    ) => {
-        $(#[$enum_meta])*
-        $vis enum $name {
-            $(
-                $(#[$variant_meta])*
-                $variant,
-            )*
-        }
-
-        impl $crate::ScpiDeserialize for $name {
-            fn deserialize(input: &mut &str) -> $crate::Result<Self> {
-                $(
-                    if let Ok(()) = $crate::match_literal(input, $literal) {
-                        return Ok(Self::$variant);
-                    }
-                )*
-                Err($crate::Error::ResponseDecoding(format!("Unexpected token for {}: `{}`", stringify!($name), input)))
-            }
-        }
-
-        impl $crate::ScpiSerialize for $name {
-            fn serialize(&self, out: &mut String) {
-                match self {
-                    $(
-                        Self::$variant => out.push_str($literal),
-                    )*
-                }
-            }
-        }
-    };
 }
 
 #[cfg(test)]
